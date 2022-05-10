8d5fccff026f6d9a3c421036df6d558aa6cead08
unknown <001kamilov@gmail.com>
2022-05-10 23:06:36 +0600

    first

---
+++ js/packages/web/src/constants/labels.ts
@@ -0,0 +1,19 @@
+export const LABELS = {
+  STORE_NAME: 'Metaplex-Arweave',
+  CONNECT_LABEL: 'Подключить кошелек',
+  GIVE_SOL: 'Получить SOL',
+  FAUCET_INFO:
+    'This faucet will help you fund your accounts outside of Solana main network.',
+  ACCOUNT_FUNDED: 'Account funded.',
+  MENU_HOME: 'Домой',
+  MENU_FAUCET: 'Faucet',
+  CONNECT_BUTTON: 'Подключиться',
+  WALLET_TOOLTIP: 'Wallet public key',
+  WALLET_BALANCE: 'Баланс кошелька',
+  SETTINGS_TOOLTIP: 'Настройки',
+  GO_BACK_ACTION: 'Назад',
+  TOTAL_TITLE: 'Всего',
+  ENTER_AMOUNT: 'Введите количество',
+  TRANSFER: 'Трансфер',
+  SET_CORRECT_WALLET_NETWORK: 'Установить сеть',
+};
